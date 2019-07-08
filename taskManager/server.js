4bd3233256929bc84ea4dbf5cf1d79a383914331
Souvik Kundu <kivtech88@gmail.com>
2019-07-08 21:07:27 +0530

    Update server.js

--- taskManager/server.js
+++ taskManager/server.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const PORT = 3000;
 const app = express();
+let tasks = [
+    "Learn JavaScript",
+    "Learn Nodejs",
+    "Learn Testing",
+    "Learn React",
+    "Learn Angular"
+
+];
+app.get('/', function(req, res) {
+    res.send('<h1>Hello World!</h1>');
+});
+app.get('/tasks', function(req, res) {
+    res.json(tasks);
+});
+app.get('/get-random-tasks', function(req, res) {
+    const randomTask = tasks[Math.floor(Math.random() * tasks.length)];
+    res.send(randomTask);
+})
 app.listen(PORT, function() {
     console.log("App is listening to the port number " +PORT);
 });
